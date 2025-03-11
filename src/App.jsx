567655d80977d6ff8702f0cd8a3b16b696d897e2
Bideri Alec <101627722+alecbideri@users.noreply.github.com>
2025-03-11 20:11:37 +0200

    Added content on new features

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./sections/Header.jsx";
 import Hero from "./sections/Hero.jsx";
+import Feautures from "./sections/Feautures.jsx";
 
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header />
       <Hero />
+      <Feautures />
     </main>
   );
 };
---
+++ src/constants/index.js
@@ -0,0 +1,24 @@
+export const features = [
+  {
+    id: "0",
+    icon: "/images/feature-1.png",
+    caption: "DEMO caption",
+    title: "Feauture 1",
+    text: "text 1",
+    button: {
+      icon: "/images/magictouch,svg",
+      title: "Watch demo",
+    },
+  },
+  {
+    id: "1",
+    icon: "/images/feature-2.png",
+    caption: "DEMO caption",
+    title: "Feauture 2",
+    text: "text 2",
+    button: {
+      icon: "/images/magictouch,svg",
+      title: "Watch demo",
+    },
+  },
+];
