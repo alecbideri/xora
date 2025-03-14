31a8c7d5647609c8dbf67bf2fa13a53fef720d4b
Bideri Alec <101627722+alecbideri@users.noreply.github.com>
2025-03-14 15:29:44 +0200

    Modified scroll to nav to the hero from the md devices

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -33,7 +33,15 @@ const Header = () => {
     >
       <div className="container flex h-14 items-center max-lg:px-5">
         <a className="lg:hidden flex-1 cursor-pointer z-2">
-          <img src="/images/xora.svg" width={115} height={55} alt="logo" />
+          <LinkScroll
+            to="hero"
+            offset={-250}
+            spy
+            smooth
+            className={clsx("transition-transform duration-500 cursor-pointer")}
+          >
+            <img src="/images/xora.svg" width={115} height={55} alt="logo" />
+          </LinkScroll>
         </a>
 
         <div
