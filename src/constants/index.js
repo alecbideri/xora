b498affb8648bbeae231bb1b10d4b227af8fadc4
Bideri Alec <101627722+alecbideri@users.noreply.github.com>
2025-03-14 14:06:16 +0200

    Added more content on the features component

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,23 +2,23 @@ export const features = [
   {
     id: "0",
     icon: "/images/feature-1.png",
-    caption: "DEMO caption",
-    title: "Feauture 1",
-    text: "text 1",
+    caption: "Easy integration",
+    title: "Work smarter not harder",
+    text: "With Xora, tedious tasks are history. Automation and smart processes bring your productivity to new heights. It's like having an extra cup of coffee, but without the jitters.",
     button: {
-      icon: "/images/magictouch,svg",
-      title: "Watch demo",
+      icon: "/images/magictouch.svg",
+      title: "Watch the demo",
     },
   },
   {
     id: "1",
     icon: "/images/feature-2.png",
-    caption: "DEMO caption",
-    title: "Feauture 2",
-    text: "text 2",
+    caption: "Secure & trustworthy",
+    title: "Sleep easy, we got your back",
+    text: "Your data security is our priority. With state-of-the-art encryption and robust privacy controls, Xora helps keeps your information secure and locked up tighter than Fort Knox.",
     button: {
-      icon: "/images/magictouch,svg",
-      title: "Watch demo",
+      icon: "/images/docs.svg",
+      title: "Read the docs",
     },
   },
 ];
--- src/sections/Feautures.jsx
+++ src/sections/Feautures.jsx
@@ -1,5 +1,6 @@
 import { Element } from "react-scroll";
 import { features } from "../constants/index.js";
+import Button from "../components/Button.jsx";
 
 const Features = () => {
   return (
@@ -7,12 +8,27 @@ const Features = () => {
       <Element name="features">
         <div className="container">
           <div className="relative flex md:flex-wrap flex-nowrap border-2 border-s3 rounded-7xl md:overflow-hidden max-md:flex-col feature-after md:g7 max-md:border-none max-md:rounded-none max-md:gap-3">
-            {features.map((feature) => (
+            {features.map(({ id, icon, title, caption, text, button }) => (
               <div
-                key={feature.id}
+                key={id}
                 className="relative z-2 md:px-10 md:pb-10 px-5 pb-5 flex-50 max-md:custom-g7 max-md:border-2 max-md:border-s3 max-md:rounded-3xl max-md:flex-320 "
               >
-                {feature.title}
+                <div className="w-full flex justify-start items-start">
+                  <div className="-ml-3 mb-12 flex items-center justify-center flex-col">
+                    <div className="w-0.5 h-16 bg-s3" />
+                    <img
+                      src={icon}
+                      className="size-28 object-contain"
+                      alt={title}
+                    />
+                  </div>
+                </div>
+                <p className="caption mb-5 max-md:mb-6">{caption}</p>
+                <h2 className="max-w-400 mb-7 h3 text-p4 max-md:mb-6 max-md:h5">
+                  {title}
+                </h2>
+                <p className="mb-11 body-1 max-md:mb-8 max-md:body-3">{text}</p>
+                <Button icon={button.icon}>{button.title}</Button>
               </div>
             ))}
           </div>
